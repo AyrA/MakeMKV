9fa221698f82b0cddf2521dd1dbd1c897c261f26
Kevin Gut <kevin.gut@bitm.sg>
2019-06-04 02:18:43 +0200

    Add structure for XML variant

---
+++ MakeMKV/MakeMKV.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MakeMKV
+{
+    [Serializable]
+    public struct MakeMKV
+    {
+        public const string UNIX_EPOCH = "1970-01-01T00:00:00Z";
+
+        /// <summary>
+        /// Current UTC date
+        /// </summary>
+        public int date;
+        /// <summary>
+        /// First moment in time the key is considered expired
+        /// </summary>
+        public int keydate;
+        /// <summary>
+        /// Earliest point in time to ask for a key again
+        /// </summary>
+        public int cache;
+        /// <summary>
+        /// MakeMKV Key
+        /// </summary>
+        public string key;
+
+        public static int ToUnixTime(DateTime T)
+        {
+            return (int)T.ToUniversalTime().Subtract(DateTime.Parse(UNIX_EPOCH).ToUniversalTime()).TotalSeconds;
+        }
+
+        public static DateTime FromUnixTime(int UnixTime)
+        {
+            return DateTime.Parse(UNIX_EPOCH).AddSeconds(UnixTime);
+        }
+    }
+}
